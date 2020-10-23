874d451fccf013724bdd0afc613bd97d6160b3fe
moderatorman <12193288+moderatorman@users.noreply.github.com>
2020-10-23 16:44:09 -0700

    Initial commit

---
+++ JoinsPlus/src/com/oldschoolminecraft/jp/JoinsPlus.java
@@ -0,0 +1,16 @@
+package com.oldschoolminecraft.jp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class JoinsPlus extends JavaPlugin
+{
+    public void onEnable()
+    {
+        System.out.println("JoinsPlus enabled.");
+    }
+    
+    public void onDisable()
+    {
+        System.out.println("JoinsPlus disabled.");
+    }
+}
